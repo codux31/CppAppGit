b90889a56516756dff67dbeb8e9085d98ae0d080
codux <seneciomoreau@gmail.com>
2021-01-12 15:44:11 +0100

    Ajout du message "Bonjour" dans le main.

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: codux
+ *
+ * Created on 12 janvier 2021, 15:29
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Bonjour" << endl;
+    return 0;
+}
+
